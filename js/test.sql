aaf7a592781d239290a5524c7c333d8eb6c97e90
isuramanchanayake <isura.14@cse.mrt.ac.lk>
2016-12-18 00:26:00 +0530

    added important funcitons

--- js/test.sql
+++ js/test.sql
@@ -50,3 +50,67 @@ end IF;
 RETURN ToTownID;
 end$$
 DELIMITER ;
+
+create view jr as select busjourneyid, routeid from busjourney;
+
+create view manchi as (select * from bookingschedule natural join jr);
+
+drop function if exists distance_between_towns;
+DELIMITER $$
+create function distance_between_towns(fromtown varchar(5), totown varchar(5), origin varchar(5), ending varchar(5), routeid int(5)) RETURNS int(3)
+BEGIN
+declare fdistance int(3);
+declare tdistance int(3);
+declare odistance int(3);
+declare edistance int(3);
+set fdistance = (select distance from routedestination where routedestination.routeid = routeid and routedestination.townid = fromtown);
+set tdistance = (select distance from routedestination where routedestination.routeid = routeid and routedestination.townid = totown);
+set odistance = (select distance from routedestination where routedestination.routeid = routeid and routedestination.townid = origin);
+set edistance = (select distance from routedestination where routedestination.routeid = routeid and routedestination.townid = ending);
+if (fdistance >= 0
+	and
+	tdistance >= 0
+	and
+	odistance >= 0
+	and
+	edistance >= 0
+	and 
+	(fdistance >= odistance
+	and
+	tdistance >= fdistance
+	and
+	edistance >= tdistance)
+	or
+	(fdistance <= odistance
+	and
+	tdistance <= fdistance
+	and
+	edistance <= tdistance)
+	) then
+	return abs(fdistance - tdistance);
+else
+	return null;
+end if;
+END$$
+DELIMITER ;
+
+drop function if exists journey_duration;
+DELIMITER $$
+create function journey_duration(busjourneyid varchar(10), traveldistance int(3)) RETURNS bigint
+BEGIN
+	declare totaltime bigint;
+	declare totaldistance int(3);
+	declare startdistance int(3);
+	declare enddistance int(3);
+	declare route int(4);
+	set totaltime = (select duration from BusJourney where BusJourney.busjourneyid = busjourneyid);
+	set route = (select routeid from BusJourney where BusJourney.busjourneyid = busjourneyid);
+	set startdistance = (select distance from routedestination, (select fromtown from BusJourney where BusJourney.busjourneyid = 4001) as tt where tt.fromtown = routedestination.townid and 6 = routedestination.routeid);
+	set enddistance = (select distance from routedestination, (select totown from BusJourney where BusJourney.busjourneyid = busjourneyid) as tt where tt.totown = routedestination.townid and route = routedestination.routeid);
+	set totaldistance = abs(startdistance - enddistance);
+	return traveldistance * (totaltime / totaldistance);
+END$$
+DELIMITER ;
+
+select f('4001', '10');
+
