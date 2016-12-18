e7d5596e123fd659f5218129b4348da0090ae474
isuramanchanayake <isura.14@cse.mrt.ac.lk>
2016-12-18 19:56:59 +0530

    added search result model

---
+++ includes/model/searchresultmodel.php
@@ -0,0 +1,177 @@
+
+<?php
+
+class SearchResultModel
+{
+	public $scheduleid;
+	public $fromtime;
+	public $fromtown;
+	public $totime;
+	public $totown;
+	public $route;
+	public $seatno;
+	public $availableseatcount;
+	public $distance;
+	public $cost;
+	public $telephone;
+	public $wifi;
+	public $curtains;
+	public $bustype;
+	public $images;
+
+	function __construct() {
+		 
+	}
+
+	function searchResultToHTML() {
+
+		$fromtime = '2:00 AM';
+  //$fromtown = 'gettah';
+		$totime = '5:00 AM';
+  //$totown = 'Kurunegala';
+		$route = '5';
+		$availableseatcount = '14';
+		$seatno = '48';
+		$distance = '91.5';
+		$cost = '340.00';
+		$busowner = 'DS Gunasekara';
+		$telephone = '+94 71 5850 028';
+  //$bustype = 'Semi-Luxury';
+		$images = array('1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg');
+		$banid = $scheduleid;
+		$wifi = True;
+		$curtains = True;
+
+		$labels = '<span class="label label-default">'.$seatno.' seats</span>';
+		if($wifi) {
+			$labels = $labels.'<span class="label label-default">Wifi</span>';
+		}
+		if($curtains) {
+			$labels = $labels.'<span class="label label-default">Curtains</span>';
+		}
+
+		switch ($bustype) {
+			case 'Normal':
+			$labels = $labels.'<span class="label label-warning">Normal</span>';
+			break;
+			case 'Semi-Luxury':
+			$labels = $labels.'<span class="label label-info">Semi-Luxury</span>';
+			break;
+			case 'Luxury':
+			$labels = $labels.'<span class="label label-success">Luxury</span>';
+			break;
+			case 'Super-Luxury':
+			$labels = $labels.'<span class="label label-primary">Super-Luxury</span>';
+			break;
+		}
+
+		return '
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>table test</title>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+			<meta name="viewport" content="width=device-width,initial-scale=1">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+			<style type="text/css">
+				.label {
+					margin-left: 5px;
+				}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+				<div style="border: 1px solid silver; border-radius: 5px">
+					<div class="row" style="padding: 5px; padding-bottom: 2px">
+
+						<div class="col-xs-4">
+							<div id="banner">';
+
+								foreach ($images as $key => $value) {
+									echo '<img class="'.$banid.' img-thumbnail" src="images/'.$value.'" style="width: 80%">';
+								}
+
+								echo '<script type="text/javascript">
+								var slideIndex = 0;
+								carousel'.$banid.'();
+
+								function carousel'.$banid.'() {
+									var i;
+									var x = document.getElementsByClassName("'.$banid.'");
+									for (i = 0; i < x.length; i++) {
+										x[i].style.display = "none"; 
+									}
+									slideIndex++;
+									if (slideIndex > x.length) {
+										slideIndex = 1;
+									} 
+									x[slideIndex-1].style.display = "block"; 
+									setTimeout(carousel'.$banid.', 2000); // Change image every 2 seconds
+								}
+							</script>
+						</div>
+					</div>
+
+					<div class="col-xs-8">
+						<div class="row">
+							<div class="col-xs-3 vcenter">
+								<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$fromtime.'</strong><br>'.$fromtown.'</p>    
+							</div><!--
+						--><div class="col-xs-3 vcenter">
+						<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$totime.'</strong><br>'.$totown.'</p>    
+					</div><!--
+				--><div class="col-xs-3 vcenter">
+				<p style="text-align: center;font-size: 10px"><strong style="font-size: 30px">'.$route.'</strong><br>route</p>
+			</div><!--
+		--><div class="col-xs-3 vcenter">
+		<p style="text-align: center;font-size: 10px"><strong style="font-size: 30px">'.$availableseatcount.' / '.$seatno.'</strong><br>available seats</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-xs-3 vcenter">
+		<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$distance.' km</strong><br>distance</p>    
+	</div><!--
+--><div class="col-xs-3 vcenter">
+<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$cost.' LKR</strong><br>ticket price / person</p>    
+</div><!--
+--><div class="col-xs-6 vcenter">
+<div class="row">
+	<p style="text-align: center; font-size: 20px">
+		<kbd><span>
+			<i class="glyphicon glyphicon-user"></i>
+		</span>'.$busowner.'</kbd>
+	</p>
+</div>
+<div class="row">
+	<p style="text-align: center; font-size: 20px"><kbd><span>
+		<i class="glyphicon glyphicon-phone"></i>
+	</span>'.$telephone.'</kbd></p>
+</div>
+</div>
+</div>
+<div class="row">
+	'.$labels.
+	'</div>
+	<div class="row" style="margin-right: 5px; text-align: center;">
+		<button class="btn btn-primary" style="margin-top: 15px; width:50%;  position: 50%;">
+			<span>
+				<i class="glyphicon glyphicon-shopping-cart"></i>
+			</span> Reserve
+		</button>
+	</div>
+</div>
+</div>
+</div>
+
+</div>
+
+</body>
+</html>';
+}
+
+}
+
+?>
