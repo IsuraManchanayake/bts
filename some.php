8571ff6fae5dda9913abbd75eb65bb3d4cf59b09
isuramanchanayake <isura.14@cse.mrt.ac.lk>
2016-12-19 01:50:12 +0530

    tested date error in php and corrected

--- some.php
+++ some.php
@@ -1,7 +1,21 @@
 <?php   
-	$name = $_REQUEST['name'];
-	echo '<table style = "border: 1px solid black">';
-	for($i = 0; $i < 5; $i++)
-	    echo "<tr><td>".$name."</td></tr>";  
-	echo '</table';
+	// $name = $_REQUEST['name'];
+	// echo '<table style = "border: 1px solid black">';
+	// for($i = 0; $i < 5; $i++)
+	//     echo "<tr><td>".$name."</td></tr>";  
+	// echo '</table';
+?>
+
+<?php
+	//echo strtotime('2016-12-16 14:34:03.32');
+	echo "Time: " . date("Y-m-d H:i:s") . "<br>\n";
+
+$shortName = exec('date +%Z');
+echo "Short timezone:" . $shortName . "<br>";
+
+$longName = timezone_name_from_abbr($shortName);
+echo "Long timezone:" . $longName . "<br>";
+
+date_default_timezone_set('Asia/Colombo');
+echo "Time: " . date("Y-m-d H:i:s", 1481857200) . "<br>\n";
 ?>
