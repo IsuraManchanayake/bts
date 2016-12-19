54178eb36b9404e3b40042c26a3b23e8966f5d16
isuramanchanayake <isura.14@cse.mrt.ac.lk>
2016-12-20 04:00:57 +0530

    ticket price made booking price

--- includes/model/searchresultmodel.php
+++ includes/model/searchresultmodel.php
@@ -23,10 +23,12 @@ class SearchResultModel
 	public $townend;
 
 	function __construct() {
-		 
+
 	}
 
 	function searchResultToHTML() {
+
+		include 'BookTicket.php';
 
 		// $fromtime = '2:00 AM';
   // //$fromtown = 'gettah';
@@ -41,12 +43,14 @@ class SearchResultModel
 		$banid = $this->scheduleid;
 		// $telephone = '+94 71 5850 028';
   // //$bustype = 'Semi-Luxury';
-		 $this->images = array('1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg');
+		$this->images = array('1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg');
 		// $banid = $scheduleid;
 		// $wifi = True;
 		// $curtains = True;
 
-		$labels = '<span class="label label-default">'.$this->seatcount.' seats</span>';
+		$fromtownid = get_townid($this->fromtown);
+
+		$labels = '<span class="label label-default" >'.$this->seatcount.' seats</span>';
 		if($this->wifi) {
 			$labels = $labels.'<span class="label label-default">Wifi</span>';
 		}
@@ -70,76 +74,61 @@ class SearchResultModel
 		}
 
 		$html = '
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>table test</title>
-			<meta charset="UTF-8">
-			<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
-			<meta name="viewport" content="width=device-width,initial-scale=1">
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-			<style type="text/css">
-				.label {
-					margin-left: 5px;
-				}
-			</style>
-		</head>
-		<body>
-			<div class="container">
-				<div style="border: 1px solid silver; border-radius: 5px">
-					<div class="row" style="padding: 5px; padding-bottom: 2px">
+			<form role="form" action="BookTicket.php" method="get" target="_blank">
+				<div class="container">
+					<div style="border: 1px solid silver; border-radius: 5px">
+						<div class="row" style="padding: 5px; padding-bottom: 2px">
 
-						<div class="col-xs-4">
-							<div id="banner">';
+							<div class="col-xs-4">
+								<div id="banner">';
 
-								 foreach($this->images as $value) {
-									$html = $html.'<img class="a'.$banid.' img-thumbnail" src="images/'.$value.'" style="width: 80%">';
-								 }
-
-								$html = $html.'<script type="text/javascript">
-								var slideIndex = 0;
-								carousel'.$banid.'();
-
-								function carousel'.$banid.'() {
-									var i;
-									var x = document.getElementsByClassName("a'.$banid.'");
-									for (i = 0; i < x.length; i++) {
-										x[i].style.display = "none"; 
+									foreach($this->images as $value) {
+										$html = $html.'<img class="a'.$banid.' img-thumbnail" src="images/'.$value.'" style="width: 80%">';
 									}
-									slideIndex++;
-									if (slideIndex > x.length) {
-										slideIndex = 1;
-									} 
-									x[slideIndex-1].style.display = "block"; 
-									setTimeout(carousel'.$banid.', 2000); // Change image every 2 seconds
-								}
-							</script>
+
+									$html = $html.'<script type="text/javascript">
+									var slideIndex = 0;
+									carousel'.$banid.'();
+
+									function carousel'.$banid.'() {
+										var i;
+										var x = document.getElementsByClassName("a'.$banid.'");
+										for (i = 0; i < x.length; i++) {
+											x[i].style.display = "none"; 
+										}
+										slideIndex++;
+										if (slideIndex > x.length) {
+											slideIndex = 1;
+										} 
+										x[slideIndex-1].style.display = "block"; 
+										setTimeout(carousel'.$banid.', 2000); // Change image every 2 seconds
+									}
+								</script>
+							</div>
 						</div>
-					</div>
 
-					<div class="col-xs-8">
-						<div class="row">
-							<div class="col-xs-3 vcenter">
-								<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->fromtime.'</strong><br>'.$this->fromtown.'</p>    
-							</div><!--
-						--><div class="col-xs-3 vcenter">
-						<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->totime.'</strong><br>'.$this->totown.'</p>    
-					</div><!--
-				--><div class="col-xs-3 vcenter">
-				<p style="text-align: center;font-size: 10px"><strong style="font-size: 30px">'.$this->route.'</strong><br>route</p>
-			</div><!--
-		--><div class="col-xs-3 vcenter">
-		<p style="text-align: center;font-size: 10px"><strong style="font-size: 30px">'.$this->availableseatcount.' / '.$this->seatcount.'</strong><br>available seats</p>
+						<div class="col-xs-8">
+							<div class="row">
+								<div class="col-xs-3 vcenter">
+									<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->fromtime.'</strong><br>'.$this->fromtown.'</p>    
+								</div><!--
+							--><div class="col-xs-3 vcenter">
+							<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->totime.'</strong><br>'.$this->totown.'</p>    
+						</div><!--
+					--><div class="col-xs-3 vcenter">
+					<p style="text-align: center;font-size: 10px"><strong style="font-size: 30px">'.$this->route.'</strong><br>route</p>
+				</div><!--
+			--><div class="col-xs-3 vcenter">
+			<p style="text-align: center;font-size: 10px"><strong style="font-size: 30px">'.$this->availableseatcount.' / '.$this->seatcount.'</strong><br>available seats</p>
+		</div>
 	</div>
-</div>
 
-<div class="row">
-	<div class="col-xs-3 vcenter">
-		<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->distance.' km</strong><br>distance</p>    
-	</div><!--
---><div class="col-xs-3 vcenter">
-<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->cost.' LKR</strong><br><strong>booking</strong> price / person</p>    
+	<div class="row">
+		<div class="col-xs-3 vcenter">
+			<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->distance.' km</strong><br>distance</p>    
+		</div><!--
+	--><div class="col-xs-3 vcenter">
+	<p style="text-align: center;font-size: 10px"><strong style="font-size: 20px">'.$this->cost.' LKR</strong><br><strong>booking</strong> price / person</p>    
 </div><!--
 --><div class="col-xs-6 vcenter">
 <div class="row">
@@ -147,12 +136,15 @@ class SearchResultModel
 		<kbd><span>
 			<i class="glyphicon glyphicon-arrow-up"></i>
 		</span>'.$this->townstart.'</kbd>
+		<kbd><span>
+		<i class="glyphicon glyphicon-arrow-down"></i>
+	</span>'.$this->townend.'</kbd>
 	</p>
 </div>
 <div class="row">
 	<p style="text-align: right; font-size: 20px; padding-right: 50px"><kbd><span>
-		<i class="glyphicon glyphicon-arrow-down"></i>
-	</span>'.$this->townend.'</kbd></p>
+		<i class="glyphicon glyphicon-phone-alt"></i>
+	</span>'.$this->telephone.'</kbd></p>
 </div>
 </div>
 </div>
@@ -160,20 +152,20 @@ class SearchResultModel
 	'.$labels.
 	'</div>
 	<div class="row" style="margin-right: 5px; text-align: center;">
-		<button class="btn btn-primary" style="margin-top: 15px; width:50%;  position: 50%;">
+	<input type="hidden" name="fromtown" value="'.$fromtownid.'">
+		<button class="btn btn-primary" style="margin-top: 2px; width:50%;  position: 50%;" name = "scheduleid" value = "'.$this->scheduleid.'">
 			<span>
 				<i class="glyphicon glyphicon-shopping-cart"></i>
 			</span> Reserve
 		</button>
+		
 	</div>
 </div>
 </div>
 </div>
 
 </div>
-
-</body>
-</html>';
+</form>';
 
 return $html;
 }
